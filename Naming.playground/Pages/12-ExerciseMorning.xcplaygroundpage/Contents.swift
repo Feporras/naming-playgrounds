15b2f89e26de9be7d7cac77bef9574c3df227345
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-13 23:15:57 -0500

    Add files via upload

---
+++ Naming.playground/Pages/12-ExerciseMorning.xcplaygroundpage/Contents.swift
@@ -0,0 +1,65 @@
+/*: 
+ ## Exercise: Fixing Your Morning
+ 
+ There’s a lot to get done before you leave home in the morning. This exercise will help you optimize your routine.
+ 
+ - callout(Exercise): Create a constant for each activity you do in the morning before leaving home: things like `brushTeeth`, `uploadPhotos`, `chooseClothes`, `shower`, `goJogging`, `finishHomework`, `fixLunch`, and so on. Think about how many minutes each activity usually takes, and assign that value to each constant.
+ */
+let brushTeeth = 4
+// Add more here...
+
+let shower = 25
+
+let chooseClothes = 5
+
+let gojogging = 30
+
+let eat = 10
+
+let walkmycutedog = 10
+
+let watchyoutube = 20
+//: - callout(Exercise): Find the total time of all the activities by adding up the constants. Try to adjust the values or add more activities until the total time looks reasonably close to the actual amount of time you spend getting ready on an average day.
+// time getting ready..
+let ttotaltimegettingready = brushTeeth + shower + chooseClothes + gojogging + eat + walkmycutedog + watchyoutube
+
+
+//: - callout(Exercise): Add up the constants again, but this time in separate groups: one group for things you have to do and another group for things you like to do.\
+//:\
+//:If there are things you don’t have to do and don't like to do, make a third group and go ahead and sum that one, too. \
+//:\
+//:Make a new constant for each group.
+
+
+
+// optional and necessary
+let optionalpartofroutine = watchyoutube
+let necessaypartofroutine = brushTeeth + shower + chooseClothes + gojogging + eat + walkmycutedog + watchyoutube
+
+
+/*:
+ See what happens to your total time spent getting ready if you tweak the durations of the different activities. How short of a shower would you have to take in order to have more time to message your friends? Or go for a longer run? How much more time would you need if you decided to spend as long as you wanted doing all the activities you like best?
+ 
+ Change the numbers until you’ve got a design for your ideal morning. What would have to change in order for you to be able to actually spend your morning time this way?
+*/
+let brushTeeth1 = 4
+
+let shower1 = 15
+
+let chooseClothes1 = 5
+
+let gojogging1 = 20
+
+let eat1 = 10
+
+let walkmycutedog1 = 10
+
+let watchyoutube1 = 20
+
+//time
+
+let routinetime = brushTeeth1 + shower1 + chooseClothes1 + gojogging1 + eat1 + walkmycutedog1 + watchyoutube1 
+
+
+
+//:[Previous](@previous)  |  page 13 of 14  |  [Next: Exercise: Good Names](@next)
